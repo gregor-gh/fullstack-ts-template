d091a24e396f5d728283f767ab2e09aac79d3f25
gregor-gh <gregor@live.co.uk>
2021-05-08 16:38:28 +0100

    First commit, completed front-end template

---
+++ client/App.tsx
@@ -0,0 +1,12 @@
+// import "./styles.css";
+
+const App = (): JSX.Element => (
+  <>
+    <h1>
+      React TypeScript Webpack Starter Template {process.env.NODE_ENV}{" "}
+      {process.env.name}
+    </h1>
+  </>
+);
+
+export default App;
---
+++ webpack/webpack.common.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./client/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "./build/frontend"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./client/index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "public",
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+};
